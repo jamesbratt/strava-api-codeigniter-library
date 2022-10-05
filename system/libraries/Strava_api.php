dd0abc934bdb26685f2edd7ec103b5ccd1b0aaaa
Rafael Amorim <3620428+rafaelmamorim@users.noreply.github.com>
2022-10-05 12:44:02 -0300

    Up in documentation and few method fix

--- system/libraries/Strava_api.php
+++ system/libraries/Strava_api.php
@@ -36,7 +36,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
-// <editor-fold desc="To do">
+/* #region ToDo */
 /*
 
  * Activities
@@ -79,7 +79,7 @@
   Upload Activity
   Get Upload
  */
-// </editor-fold>
+/* #endregion */
 
 
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -117,9 +117,12 @@ class CI_strava_api {
     }
 
     /**
-     * A function for requesting an oath token from the strava api  
-     * The token is then used to authenticate further api calls 
+     * A function for requesting an oath token from the strava api. The token is then used to authenticate further api calls.
      * Pass the strava redirect url from a controller  
+     * 
+     * @param string $url
+     * 
+     * @return string
      */
     public function getToken($url) {
         /**
@@ -159,18 +162,27 @@ class CI_strava_api {
         curl_close($get_token);
     }
 
-    // <editor-fold desc="Activities">
+    /* #region Activities*/
 
     /**
-     * Returns the activities of an athlete for a specific identifier.
+     * Returns the activities of an athlete for a specific identifier. Requires activity:read. Only Me activities will be filtered out unless requested by a token with activity:read_all.
      * 
      * @param string $token
-     * @param int $per_page
-     * @param int $page
+     * @param int $per_page Number of items per page. Defaults to 30.
+     * @param int $page Page number. Defaults to 1.
+     * @param int $before An epoch timestamp to use for filtering activities that have taken place before a certain time.
+     * @param int $after An epoch timestamp to use for filtering activities that have taken place after a certain time.
+     * 
      * @return string
      */
-    public function getListOfActivities($token, $page = 1, $per_page = 30) {
+    public function getListOfActivities($token, $page = 1, $per_page = 30, $before = 0, $after = 0) {
         $headers = array('Authorization: Bearer ' . $token, 'per_page=' . $per_page, 'page=' . $page);
+        if ($before > 0){
+            array_push($headers,'before=' . $before);
+        }
+        if ($after > 0){
+            array_push($headers,'after=' . $after);
+        }
         $curl_handler = curl_init();
         $this->CurlOptions($curl_handler, $headers, $this->ci->config->item('list_athlete_activities'));
         $activityResponse = curl_exec($curl_handler);
@@ -184,15 +196,22 @@ class CI_strava_api {
     }
 
     /**
-     * Get an activity. $token and $idActivity must belong to same athlete
+     * Returns the given activity that is owned by the authenticated athlete. Requires activity:read for Everyone and Followers activities. Requires activity:read_all for Only Me activities. 
+     * Parameters token and idActivity must belong to same athlete
      * 
-     * @param string $token
-     * @param string $idActivity
+     * @param string $token Token from athlete 
+     * @param string $idActivity The identifier of the activity.
+     * @param bool $all_efforts To include all segments efforts. Default is true
      * @return string
      * 
      */
-    public function getActivity($token, $idActivity) {
-        $headers = array('Authorization: Bearer ' . $token, 'include_all_efforts=true');
+    public function getActivity($token, $idActivity, $all_efforts=true) {
+        $headers = array('Authorization: Bearer ' . $token);
+        if($all_efforts){
+            array_push($headers,'include_all_efforts=true');
+        } else {
+            array_push($headers,'include_all_efforts=false');
+        }
         $curl_handler = curl_init();
 
         $url = str_replace("{id}", $idActivity, $this->ci->config->item('get_activities_url'));
@@ -209,12 +228,13 @@ class CI_strava_api {
     }
 
     /**
-     * Get an activity comments. $token and $idActivity must belong to same athlete
+     * Returns the comments on the given activity. Requires activity:read for Everyone and Followers activities. Requires activity:read_all for Only Me activities. 
+     * Parameters token and idActivity must belong to same athlete.
      * 
-     * @param string $token
-     * @param string $idActivity
-     * @param int $per_page
-     * @param int $page
+     * @param string $token Token from athlete
+     * @param string $idActivity The identifier of the activity.
+     * @param int $per_page Number of items per page. Defaults to 200.
+     * @param int $page Page number. Defaults to 1.
      * @return string
      */
     public function getActivityComments($token, $idActivity, $per_page = 200, $page = 1) {
@@ -234,12 +254,13 @@ class CI_strava_api {
     }
 
     /**
-     * Get an activity kudoers. $token and $idActivity must belong to same athlete
+     * Returns the athletes who kudoed an activity identified by an identifier. Requires activity:read for Everyone and Followers activities. Requires activity:read_all for Only Me activities.
+     * Parameters token and idActivity must belong to same athlete
      * 
-     * @param string $token
-     * @param string $idActivity
-     * @param int $per_page
-     * @param int $page
+     * @param string $token Token from athlete
+     * @param string $idActivity The identifier of the activity.
+     * @param int $per_page Number of items per page. Defaults to 200.
+     * @param int $page Page number. Defaults to 1.
      * @return string
      */
     public function getActivityKudoers($token, $idActivity, $per_page = 200, $page = 1) {
@@ -260,10 +281,10 @@ class CI_strava_api {
     }
 
     /**
-     * Returns the laps of an activity identified by an identifier.
+     * Returns the laps of an activity identified by an identifier. Requires activity:read for Everyone and Followers activities. Requires activity:read_all for Only Me activities.
      * 
-     * @param string $token
-     * @param string $idActivity
+     * @param string $token Token from athlete
+     * @param string $idActivity The identifier of the activity.
      * @return string
      */
     public function getActivityLaps($token, $idActivity) {
@@ -282,8 +303,8 @@ class CI_strava_api {
         return $activityResponse;
     }
 
-    // </editor-fold>
-    // <editor-fold desc="Atlhetes">   
+    /* #endregion */
+    /* #region Atlhetes*/
     /**
      * Returns the currently authenticated athlete.
      * 
@@ -306,7 +327,7 @@ class CI_strava_api {
     }
 
     /**
-     * Returns the the authenticated athlete's heart rate and power zones.
+     * Returns the the authenticated athlete's heart rate and power zones. Requires profile:read_all.
      * 
      * @param string $token
      * @return string
@@ -327,16 +348,14 @@ class CI_strava_api {
     }
 
     /**
-     * Returns the activity stats of an athlete.
+     * Returns the activity stats of an athlete. Only includes data from activities set to Everyone visibilty.
      * 
-     * @param string $token
-     * @param string $id
-     * @param int $page
-     * @param int $per_page
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the athlete. Must match the authenticated athlete.
      * @return string
      */
-    public function getAthleteStats($token, $id, $page = 1, $per_page = 30) {
-        $headers = array('Authorization: Bearer ' . $token, 'page=' . $page, 'per_page=' . $per_page);
+    public function getAthleteStats($token, $id) {
+        $headers = array('Authorization: Bearer ' . $token);
         $curl_handler = curl_init();
         $url = str_replace("{id}", $id, $this->ci->config->item('get_athlete_stats_url'));
 
@@ -351,18 +370,18 @@ class CI_strava_api {
         return $activityResponse;
     }
 
-    // </editor-fold>
-    // <editor-fold desc="Clubs">
+    /* #endregion */
+    /* #region Clubs*/
     /**
      * Retrieve recent activities from members of a specific club. 
      * The authenticated athlete must belong to the requested club in 
      * order to hit this endpoint. Pagination is supported. 
      * Enhanced Privacy Mode is respected for all activities.
      * 
-     * @param string $token
-     * @param string $id
-     * @param int $page
-     * @param int $per_page
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the club.
+     * @param int $page Page number. Defaults to 1.
+     * @param int $per_page Number of items per page. Defaults to 30.
      * @return string
      */
     public function getListOfClubActivities($token, $id, $page = 1, $per_page = 30) {
@@ -385,10 +404,10 @@ class CI_strava_api {
     /**
      * Returns a list of the administrators of a given club.
      * 
-     * @param string $token
-     * @param string $id
-     * @param int $page
-     * @param int $per_page
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the club.
+     * @param int $page Page number. Defaults to 1.
+     * @param int $per_page Number of items per page. Defaults to 30.
      * @return string
      */
     public function getClubAdministrators($token, $id, $page = 1, $per_page = 30) {
@@ -411,8 +430,8 @@ class CI_strava_api {
     /**
      * Returns a given club using its identifier.
      * 
-     * @param string $token
-     * @param string $id
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the club.
      * @return string
      */
     public function getClub($token, $id) {
@@ -435,10 +454,10 @@ class CI_strava_api {
     /**
      * Returns a list of the athletes who are members of a given club.
      * 
-     * @param string $token
-     * @param string $id
-     * @param int $page
-     * @param int $per_page
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the club.
+     * @param int $page Page number. Defaults to 1.
+     * @param int $per_page Number of items per page. Defaults to 30.
      * @return string
      */
     public function getClubMembers($token, $id, $page = 1, $per_page = 30) {
@@ -461,9 +480,9 @@ class CI_strava_api {
     /**
      * Returns a list of the clubs whose membership includes the authenticated athlete.
      * 
-     * @param string $token
-     * @param int $page
-     * @param int $per_page
+     * @param string $token Token from athlete
+     * @param int $page Page number. Defaults to 1.
+     * @param int $per_page Number of items per page. Defaults to 30.
      * @return string
      */
     public function getAthleteClubs($token, $page = 1, $per_page = 30) {
@@ -482,13 +501,13 @@ class CI_strava_api {
         return $activityResponse;
     }
 
-    // </editor-fold>
-    // <editor-fold desc="Gears">
+    /* #endregion */
+    /* #region Gears*/
     /**
      * Returns an equipment using its identifier.
      * 
-     * @param string $token
-     * @param string $id
+     * @param string $token Token from athlete
+     * @param string $id The identifier of the gear.
      * @return string
      */
     public function getGear($token, $id) {
@@ -511,8 +530,8 @@ class CI_strava_api {
         return $activityResponse;
     }
 
-    // </editor-fold>
-    // <editor-fold desc="CURL config">
+    /* #endregion */
+    /* #region CURL config */
     private function CurlOptions($handler, $headers, $url) {
         curl_setopt($handler, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($handler, CURLOPT_URL, $url);
@@ -520,5 +539,5 @@ class CI_strava_api {
         curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
     }
 
-    // </editor-fold>
+    /* #endregion */
 }
